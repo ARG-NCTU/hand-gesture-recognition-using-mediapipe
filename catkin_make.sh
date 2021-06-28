7ca5b140c60a93ddd7b27ffc7e36a7a6f3a45fcc
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-28 16:48:49 +0800

    06_28

--- catkin_make.sh
+++ catkin_make.sh
@@ -1,5 +1,6 @@
 cd ~/hand-gesture-recognition-using-mediapipe/catkin_ws
 catkin_make
 
+
 source $HOME/hand-gesture-recognition-using-mediapipe/catkin_ws/devel/setup.bash
 cd ~/hand-gesture-recognition-using-mediapipe

8e1889706e5d9ad12e4dcf5471d02a3da2f85d28
DiaboloKiat <DiaboloKiat@gmail.com>
2021-06-28 16:51:22 +0800

    06_28

--- bag/record_D435.sh
+++
@@ -1,3 +0,0 @@
-rosbag record /camera/color/camera_info /camera/color/image_raw /camera/depth/camera_info /camera/depth/color/points /camera/depth/image_rect_raw /tf /tf_static
-
-
--- catkin_make.sh
+++
@@ -1,6 +0,0 @@
-cd ~/hand-gesture-recognition-using-mediapipe/catkin_ws
-catkin_make
-
-
-source $HOME/hand-gesture-recognition-using-mediapipe/catkin_ws/devel/setup.bash
-cd ~/hand-gesture-recognition-using-mediapipe

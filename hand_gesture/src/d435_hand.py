96eb350561f383ba45656167a9c3d18865db6189
DiaboloKiat <DiaboloKiat@gmail.com>
2021-07-09 15:30:30 +0800

    d435 hand gesture on hand-gesture-recognition-using-mediapipe

---
+++ hand_gesture/src/d435_hand.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python3
+
+import os
+import numpy as np
+import cv2
+import math
+import time
+import json
+import mediapipe as mp      # https://google.github.io/mediapipe/solutions/hands.html
+
+
+# ROS
+import rospy
+import roslib
+import rospkg
+import message_filters
+
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image, CameraInfo
+from std_msgs.msg import String
+from utils import CvFpsCalc
+
+class Detection(object):
+    def __init__(self):
+        self.bridge = CvBridge()
+
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.mp_hands = mp.solutions.hands
+
+        # For webcam input:
+        self.hands = self.mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence = 0.7, min_tracking_confidence = 0.7)
+
+        # FPS Measurement
+        self.cv_fps_calc = CvFpsCalc(buffer_len=5)
+
+        # ROS Publisher & Subscriber
+        self.sub_img = message_filters.Subscriber('/camera/color/image_raw', Image)
+        self.pub_img = rospy.Publisher('/camera/hand_detectec/image_raw', Image, queue_size = 10)
+
+        rospy.loginfo('Detection node ready.')
+
+        ts = message_filters.TimeSynchronizer([self.sub_img], 1)
+        ts.registerCallback(self.callback)
+
+    def callback(self, rgb):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(rgb, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        fps = self.cv_fps_calc.get()
+
+
+        # Flip the image horizontally for a later selfie-view display, and convert
+        # the BGR image to RGB.
+        image = cv2.cvtColor(cv2.flip(cv_image, 1), cv2.COLOR_BGR2RGB)
+    
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        results = self.hands.process(image)
+
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                self.mp_drawing.draw_landmarks(image, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+
+        # Battery status and image rendering
+        cv2.putText(image, "fps:{}".format(fps), (5, 720 - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        self.pub_img.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+        cv2.imshow('hand_pose_d435', image)
+        cv2.waitKey(3)
+
+    def shutdown_cb(self):
+        rospy.loginfo("Node shutdown")
+
+if __name__ == "__main__":
+    rospy.init_node("d435_hand_gesture_node", anonymous=False)
+    node = Detection()
+    cv2.destroyAllWindows()
+    rospy.on_shutdown(node.shutdown_cb)
+
+    rospy.spin()
